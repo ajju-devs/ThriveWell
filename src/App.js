8caf45a29ebc6510c10919ea97aa541a5685dd2e
Ajay Kumar <theajaykumar29@gmail.com>
2024-08-06 02:58:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Surveys from './components/Surveys';
 import Contact from './components/Contact';
 import Resources from './components/Resources';
 import Tips from './components/Tips';
-import Navbar from './components/Navbar'; // Import Navbar component
+import Navbar from './components/Navbar';
 import Logo from './components/Logo';
 import Footer from './components/Footer';
 import './App.css';
@@ -19,21 +19,23 @@ function App() {
     <Router>
       <div className="App">
         <div className="App-logo-container">
-          <Logo /> {/* Logo component with updated styles */}
+          <Logo />
         </div>
-        <Navbar /> {/* Use Navbar component for navigation */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/statistics" element={<Statistics />} />
-          <Route path="/quizzes" element={<Quizzes />} />
-          <Route path="/surveys" element={<Surveys />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/tips" element={<Tips />} /> {/* Add route for Tips */}
-        </Routes>
-        <Footer /> {/* Add the Footer component here */}
+        <Navbar />
+        <div className="App-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/quizzes" element={<Quizzes />} />
+            <Route path="/surveys" element={<Surveys />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/tips" element={<Tips />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
